b3a73391d5e582fa5e3d30710c0178c11515d289
Sthefania <sthefaniarroyo@gmail.com>
2026-06-03 12:33:54 -0600

    Leccion 4

--- vistas/vista_superior.php
+++ vistas/vista_superior.php
@@ -1,26 +1,29 @@
-AMA SBK 
-<!DOCTYPE html>
-<html lang="en">
-   <head>
-       <meta charset="UTF-8">
-       <meta http-equiv="X-UA-Compatible" content="IE=edge">
-       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <link rel="stylesheet" href="./style/main.css">
-       <title>AMA SBK </title>
-   </head>
-  <body>
-    <menu>
-         <div class="menu_izquierdo">
-            <h1>AMA SUPER BIKE COSTA RICA</h1>
-         <div>
-        <div class="menu_derecho">
-            <a href="./index.php">Acerca de</a>
-            <a href="./Inscripciones.php">Inscripciones</a>
-            <a href="./Fotos.php">Fotos</a>
-            <a href="./Membresia.php">Membresía</a>
-            <a href="./Asesoramiento.php">Asesoramiento</a>
-            <a href="./Llantas.php">Llantas</a>
-           
-        </div>
-    <menu>
 
+<!DOCTYPE html>
+<html lang="en"> 
+   <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./Style/main.css">
+        <title>AMA SUPER BIKE CR</title>
+   </head>
+   <body>
+       <menu>
+           <div class="menu_izquierdo">
+                <img src="./img/menu2.png" alt="AMA SUPER BIKE CR">
+           </div>
+           <div class="menu_derecho">
+               <a href="./Acerca de.php">Acerca de</a>
+               <a href="./contacto.php">Contacto</a>
+               <a href="./inscripciones.php">Inscripciones</a>
+               <a href="./membresia.php">Membresía</a>
+               <a href="./fotos.php">Fotos</a>
+               <a href="./Asesoramiento.php">Asesoramiento</a>
+               <a href="./llantas.php">Llantas</a>
+           </div>
+       </menu>  
+         
+           
+        
+        
