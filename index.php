b21672b26acacd9a2d9825855bcdce1e7ab519fb
Sthefania <sthefaniarroyo@gmail.com>
2026-06-03 09:52:16 -0600

    Leccion_4

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php require_once "./vistas/vista_superior.php"?>
 
 
-index
-
-
-
-
 
 <?php require_once "./vistas/vista_inferior.php"?>
--- vistas/vista_superior.php
+++ vistas/vista_superior.php
@@ -1,1 +1,22 @@
-Vista SUPERIOR
+MOTOGP
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+       <meta charset="UTF-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>Tercera_I</title>
+       <link rel="stylesheet" href="../style/main.css">
+   </head>
+  <body>
+    <menu>
+         <divclass="menu_izquierdo">
+         <div>
+        <div class="menu_derecho">
+            <a href="#">Yamaha</a>
+            <a href="#">Ducati</a>
+            <a href="#">Aprilia</a>
+            <a href="#">Honda</a>
+        </div>
+    <menu>
+</html>
