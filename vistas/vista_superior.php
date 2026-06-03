934d776e6f248f492bf599a92b597b37668a87f2
Sthefania <sthefaniarroyo@gmail.com>
2026-06-03 10:31:52 -0600

    Leccion_4

--- vistas/vista_superior.php
+++ vistas/vista_superior.php
@@ -1,22 +1,26 @@
-MOTOGP
+AMA SBK 
 <!DOCTYPE html>
 <html lang="en">
    <head>
        <meta charset="UTF-8">
        <meta http-equiv="X-UA-Compatible" content="IE=edge">
        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <title>Tercera_I</title>
-       <link rel="stylesheet" href="../style/main.css">
+       <link rel="stylesheet" href="./style/main.css">
+       <title>AMA SBK </title>
    </head>
   <body>
     <menu>
-         <divclass="menu_izquierdo">
+         <div class="menu_izquierdo">
+            <h1>AMA SUPER BIKE COSTA RICA</h1>
          <div>
         <div class="menu_derecho">
-            <a href="#">Yamaha</a>
-            <a href="#">Ducati</a>
-            <a href="#">Aprilia</a>
-            <a href="#">Honda</a>
+            <a href="./index.php">Acerca de</a>
+            <a href="./Inscripciones.php">Inscripciones</a>
+            <a href="./Fotos.php">Fotos</a>
+            <a href="./Membresia.php">Membresía</a>
+            <a href="./Asesoramiento.php">Asesoramiento</a>
+            <a href="./Llantas.php">Llantas</a>
+           
         </div>
     <menu>
-</html>
+
